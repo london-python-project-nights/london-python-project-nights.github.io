9814d90fb78d0f7ca2e2d92eb9b606fe465a457f
Alistair Broomhead <alistair.broomhead@gmail.com>
2015-06-08 19:48:08 +0100

    Add events section, refactor asset tree
    There's no nav lint to the events section yet, until I've got some people's opinions.
    
    Add a little extra styling

--- build.py
+++ build.py
@@ -1,20 +1,54 @@
+# -*- coding: utf-8 -*-
 import os
 from jinja2 import Environment, PackageLoader
 
-loader = PackageLoader('site', 'templates')
-provider = loader.provider
-provider.module_path = os.path.dirname(os.path.abspath(__file__))
+LOADER = PackageLoader('site', 'templates')
+PROVIDER = LOADER.provider
+PROVIDER.module_path = os.path.dirname(os.path.abspath(__file__))
 
-env = Environment(loader=loader)
+ENV = Environment(loader=LOADER)
 
-def write(template_path, output_path):
+
+def write(template_path, output_path, **context):
     with open(output_path, 'w') as output:
-        template = env.get_template(template_path)
-        output_string = template.render()
-        output.write(output_string)
+        template = ENV.get_template(template_path)
+        output_string = template.render(**context)
+        output.write(output_string.encode('utf-8'))
 
 
-write('html/home.html', 'index.html')
-write('html/coc.html', 'code-of-conduct.html')
-write('html/forum.html', 'mail.html')
-write('html/twitter_embed.html', 'tweets.html')
+def get_events(events_root):
+
+    for season in os.listdir(events_root):
+        if not os.path.isdir(os.path.join(events_root, season)):
+            continue
+        for filename in os.listdir(os.path.join(events_root, season)):
+            if not os.path.isfile(os.path.join(events_root, season, filename)):
+                continue
+            episode, ext = os.path.splitext(filename)
+            if ext == '.html':
+                yield dict(
+                    template_path='html/events/{}/{}'.format(season, filename),
+                    output_path='events/s{}e{}'.format(season, filename),
+                    season=season,
+                    episode=episode,
+                    file=filename
+                )
+
+
+def main():
+
+    write('html/home.html', 'index.html')
+    write('html/coc.html', 'code-of-conduct.html')
+    write('html/forum.html', 'mail.html')
+    write('html/twitter_embed.html', 'tweets.html')
+
+    events = []
+
+    for event in get_events('templates/html/events'):
+        events.append(event)
+        write(**event)
+
+    write('html/events.html', 'events/index.html', events=events)
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,21 +1,45 @@
 import os
 
 import bottle
+from threading import Thread
+from time import sleep
 
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 APP = bottle.Bottle()
 
 
-@APP.get('/')
-def root():
-    return bottle.static_file('index.html', ROOT)
+def continuous_build(should_run):
+    """
+
+    :type should_run: Event
+    """
+    import build
+
+    while should_run['now']:
+        build.main()
+        sleep(1)
 
 
 @APP.get('/:path#.*#')
 def static(path):
+    print path, '->',
+    if path == '' or os.path.isdir(path):
+        path += '/index.html'
+    elif os.path.isfile(path):
+        pass
+    elif not path.endswith('.html'):
+        path += '.html'
     print path
     return bottle.static_file(path, ROOT)
 
 if __name__ == "__main__":
-    APP.run(reloader=True)
+    running = {'now': True}
+    builder = Thread(target=continuous_build, args=(running,))
+    builder.start()
+
+    try:
+        APP.run(reloader=True)
+    except:
+        running['now'] = False
+        raise
